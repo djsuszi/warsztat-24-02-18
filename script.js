c878f34ffb19c9cfef7be9e99e0019f700aa8e81
djsuszi <pkassin@gmail.com>
2018-02-24 13:17:14 +0100

    loop

--- script.js
+++ script.js
@@ -1,48 +1,12 @@
-console.log('Hello Piotr!')
-//string
-var textVariable = "Matrusz"
-console.log(textVariable)
-//number
-var numberVariable=43
-console.log(numberVariable)
 
+function hello(name,ilerazy)
+{
 
-var booleanVariable = true
-console.log(booleanVariable)
-
-var nullVariable = null
-console.log(nullVariable)
-
-var undefinedVariable = undefined
-console.log(undefinedVariable)
-
-//object with data
-var simplestObj = { 
-    name: "Piotr",
-    lastname: "Kassin"
-}
-console.log(simplestObj)
-console.log(simplestObj.name)
-
-var nastedObj = { 
-    name: "Piotr",
-    car: {
-        brand: "Audi",
-        model: 3
+    for(var i = 0; i<ilerazy; i++)
+    {
+    console.log("Hello "+name)
     }
-
 }
-console.log(nastedObj)
-console.log("Brand: "+nastedObj.car.brand)
-nastedObj.car.brand=99
-console.log("Brand2: "+nastedObj.car.brand)
-
-//arrays
-var simpleArray = ["jeden",
-    {name:'"Ala"', array: [1,4,"wow"]},
-3,4]
-console.log(simpleArray)
-console.log(simpleArray[0])
-console.log(simpleArray[1].name)
-console.log(simpleArray[1]["name"])
-console.log(simpleArray[1].array[2])
+hello("Piotr",5)
+// funkcje JS  foreach, map, filter, reduce
+// "fun fun function"  kanał na YT

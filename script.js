212e18febc0d3cb2dc9eb74f1a1c760ae0e4f1e2
djsuszi <pkassin@gmail.com>
2018-02-24 15:02:45 +0100

    onclick body

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
-function hello(name,ilerazy)
+(function (){
+    
+function kassinhello(name,ilerazy)
 {
 
     for(var i = 0; i<ilerazy; i++)
@@ -7,6 +8,51 @@ function hello(name,ilerazy)
     console.log("Hello "+name)
     }
 }
-hello("Piotr",5)
+var kassin2=kassinhello
+
+// hello("Piotr",5)
 // funkcje JS  foreach, map, filter, reduce
 // "fun fun function"  kanał na YT
+
+
+var secondsCounter=0
+
+var intervalId = setInterval(
+    function () {
+        var data = new Date().getTime();
+        var wewZmienna="asd"
+//        var teraz = data.getTime();
+        // console.clear()
+        console.log('minela sekunda '+secondsCounter++ +' '+Math.round(data/1000))
+//clearInterval(intervalId)
+            },
+    1000
+)
+
+setTimeout(
+    function()  {
+            clearInterval(intervalId)
+                },
+    5000
+)
+
+//debugger  włączenie debuga
+
+var body=document.querySelector('body')
+
+console.log(body)
+//console.dir(body)  bardziej szczegółowo
+
+body.addEventListener(
+    'click',
+    function() {
+        console.log('klik!')
+
+    }
+)
+
+}) () //koniec funkcji anonimowej
+
+
+
+//console.log(dupa)

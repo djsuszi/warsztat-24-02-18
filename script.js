db691ea10b80a287e3206f78381b500d86074cfc
djsuszi <pkassin@gmail.com>
2018-02-24 12:50:41 +0100

    zmienne wszystkie

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(textVariable)
 var numberVariable=43
 console.log(numberVariable)
 
-//boolean
+
 var booleanVariable = true
 console.log(booleanVariable)
 
@@ -15,3 +15,34 @@ console.log(nullVariable)
 
 var undefinedVariable = undefined
 console.log(undefinedVariable)
+
+//object with data
+var simplestObj = { 
+    name: "Piotr",
+    lastname: "Kassin"
+}
+console.log(simplestObj)
+console.log(simplestObj.name)
+
+var nastedObj = { 
+    name: "Piotr",
+    car: {
+        brand: "Audi",
+        model: 3
+    }
+
+}
+console.log(nastedObj)
+console.log("Brand: "+nastedObj.car.brand)
+nastedObj.car.brand=99
+console.log("Brand2: "+nastedObj.car.brand)
+
+//arrays
+var simpleArray = ["jeden",
+    {name:'"Ala"', array: [1,4,"wow"]},
+3,4]
+console.log(simpleArray)
+console.log(simpleArray[0])
+console.log(simpleArray[1].name)
+console.log(simpleArray[1]["name"])
+console.log(simpleArray[1].array[2])

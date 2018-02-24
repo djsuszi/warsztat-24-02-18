da4e127511c8c48ce92bbfd28b330e5acee8c110
djsuszi <pkassin@gmail.com>
2018-02-24 16:36:27 +0100

    działający kret

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-(function (){
+
+function test()  {
     
 function kassinhello(name,ilerazy)
 {
-
     for(var i = 0; i<ilerazy; i++)
     {
     console.log("Hello "+name)
@@ -10,13 +10,9 @@ function kassinhello(name,ilerazy)
 }
 var kassin2=kassinhello
 
-// hello("Piotr",5)
 // funkcje JS  foreach, map, filter, reduce
 // "fun fun function"  kanał na YT
-
-
 var secondsCounter=0
-
 var intervalId = setInterval(
     function () {
         var data = new Date().getTime();
@@ -51,8 +47,84 @@ body.addEventListener(
     }
 )
 
-}) () //koniec funkcji anonimowej
+} //(); //koniec funkcji anonimowej
 
+(function (){
+ 
+    var points
+    var time 
+    var mole 
+    var gameIntervalId
 
+    function addPoint() {
+            document.querySelector('body').style.backgroundColor='red'
+        points++;
+        //document.body.getElementsByClassName('points')[0].innerText=points;
+        //console.log('Dodano punkt '+points)
+        //makeMole()
+        displayPoints(points)
+        document.querySelector('body').style.backgroundColor='green'
+    }    
 
-//console.log(dupa)
+    function displayPoints (pointsParam)    {
+        var pointsContainer= document.querySelector('.points')
+        pointsContainer.innerText='Punkty:'+pointsParam
+    }
+
+    function displayTime (timeParam)    {
+        var timeContainer= document.querySelector('.time')
+        timeContainer.innerText='Czas:'+timeParam
+    }
+
+    function makeMole() {
+        var molePosX=Math.round( Math.random() * (window.innerWidth  - window.innerHeight/10) )
+        var molePosY=Math.round( Math.random() * (window.innerHeight - window.innerHeight/10) )
+        var mole=document.createElement('div')
+        mole.classList.add('mole')
+        mole.style.left=molePosX+'px'
+        mole.style.top=molePosY+'px'
+
+        mole.addEventListener('click', function ()
+            {
+                mole.remove()
+                console.log('Kret Klik')
+                addPoint()
+            }
+            )
+        document.querySelector('body').appendChild(mole)
+        return mole
+    }
+
+    function reduceTime() {
+        time--
+        displayTime(time)
+
+    }
+    function init()
+    {
+        points=0
+        time=10
+        mole=makeMole()
+        displayPoints(points)
+        displayTime(time)
+
+       /* moletime=setInterval( function() {
+                    mole.remove()
+            },10000)
+        */
+
+        gameIntervalId = setInterval( 
+        function() { 
+            mole.remove()
+            mole=makeMole()
+            reduceTime()
+            console.log('Kret timeout')
+        },
+        1500
+        )
+    }
+
+    //for (i=0; i<10; i++) {  makeMole(); }
+init()
+
+}) () //koniec funkcji anonimowej drugiej

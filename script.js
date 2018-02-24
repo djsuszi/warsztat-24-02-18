9931de1a92e1920cc1d0f3cfee590c69551be004
djsuszi <pkassin@gmail.com>
2018-02-24 12:28:50 +0100

    js typy

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello Piotr!')
+//string
+var textVariable = "Matrusz"
+console.log(textVariable)
+//number
+var numberVariable=43
+console.log(numberVariable)
+
+//boolean
+var booleanVariable = true
+console.log(booleanVariable)
+
+var nullVariable = null
+console.log(nullVariable)
+
+var undefinedVariable = undefined
+console.log(undefinedVariable)
